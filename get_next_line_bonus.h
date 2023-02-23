7c6a0c98778a0c9ab6e7049dc11db547a8e9fc2e
BeAzarym <calvinchabeau01@gmail.com>
2023-02-23 14:41:00 +0100

    fix norminette

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: cchabeau <cchabeau@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:26:19 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/02/23 14:26:30 by cchabeau         ###   ########.fr       */
+/*   Updated: 2023/02/23 14:40:02 by cchabeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 500
 # endif
@@ -29,4 +29,5 @@ size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
 char	*ft_strjoin(char *s1, char *s2, int *check_newline);
 char	*ft_strdup(char *str, int *check_newline);
 char	*get_next_line(int fd);
+
 #endif
